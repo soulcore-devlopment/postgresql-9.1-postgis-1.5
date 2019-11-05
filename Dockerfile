94bde4164031eeede746da7641a0e10136a0bf74
guilherme.prado@soulcore.com.br <guilherme.prado@soulcore.com.br>
2019-11-05 20:32:48 -0300

    This version not and ther other don't work with volume bind. It's are related with some event flow + permission on the bind volume. Looking at the folder docker volume the permission off postgresql are with this setup This a docker/volume location files
    user@vm-dev-01:~/Documents$ sudo ls -hl /mnt/data/dockerfiles/volumes/pg9_102/
    total 4.0K
    drwx------ 13 systemd-network systemd-resolve 4.0K Nov  5 17:32 _data
    user@vm-dev-01:~/Documents$ sudo ls -hl /mnt/data/dockerfiles/volumes/pg9_102/_data
    total 56K
    drwx------ 6 systemd-network systemd-resolve 4.0K Nov  5 17:32 base
    drwx------ 2 systemd-network systemd-resolve 4.0K Nov  5 17:33 global
    drwx------ 2 systemd-network systemd-resolve 4.0K Nov  5 17:32 pg_clog
    drwx------ 4 systemd-network systemd-resolve 4.0K Nov  5 17:32 pg_multixact
    drwx------ 2 systemd-network systemd-resolve 4.0K Nov  5 17:32 pg_notify
    drwx------ 2 systemd-network systemd-resolve 4.0K Nov  5 13:00 pg_serial
    drwx------ 2 systemd-network systemd-resolve 4.0K Nov  5 17:33 pg_stat_tmp
    drwx------ 2 systemd-network systemd-resolve 4.0K Nov  5 17:32 pg_subtrans
    drwx------ 2 systemd-network systemd-resolve 4.0K Nov  5 13:00 pg_tblspc
    drwx------ 2 systemd-network systemd-resolve 4.0K Nov  5 13:00 pg_twophase
    -rw------- 1 systemd-network systemd-resolve    4 Nov  5 13:00 PG_VERSION
    drwx------ 3 systemd-network systemd-resolve 4.0K Nov  5 17:32 pg_xlog
    -rw------- 1 systemd-network systemd-resolve  133 Nov  5 17:32 postmaster.opts
    -rw------- 1 systemd-network systemd-resolve   89 Nov  5 17:32 postmaster.pid
    lrwxrwxrwx 1 systemd-network systemd-resolve   36 Nov  5 13:00 server.crt -> /etc/ssl/certs/ssl-cert-snakeoil.pem
    lrwxrwxrwx 1 systemd-network systemd-resolve   38 Nov  5 13:00 server.key -> /etc/ssl/private/ssl-cert-snakeoil.key
    -rw-r--r-- 1 systemd-network systemd-resolve    0 Nov  5 17:32 wasrun

--- Dockerfile
+++ Dockerfile
@@ -4,31 +4,46 @@ MAINTAINER "Étienne Loks <etienne.loks@iggdrasil.net>"
 
 ENV PG_VERSION=9.1
 ENV POSTGIS_VERSION=1.5
-ENV PG_DATA=/var/lib/postgresql/data
+ENV PG_DATA=/var/lib/postgresql/$PG_VERSION/main
+ENV PG_RUN=$PG_DATA/wasrun
+
 ENV POSTGRES_DB=postgres
 ENV POSTGRES_USER=postgres
 ENV POSTGRES_PASSWORD=postgres
- 	
+
 RUN apt-get update && apt-get install -y postgresql postgresql-client postgresql-contrib postgis postgresql-$PG_VERSION-postgis postgresql-plpython postgresql-plpython-$PG_VERSION nano && rm -rf /var/lib/apt/lists/*
 
+RUN sed -i 's/data_directory/##data_directory/g' /etc/postgresql/$PG_VERSION/main/postgresql.conf
+
 RUN echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/$PG_VERSION/main/pg_hba.conf
+RUN echo "data_directory = '${PG_DATA}'" >> /etc/postgresql/$PG_VERSION/main/postgresql.conf
 RUN echo "listen_addresses='*'" >> /etc/postgresql/$PG_VERSION/main/postgresql.conf
 
-#ADD docker-entrypoint.sh /docker-entrypoint.sh
-#RUN chmod -v +x /docker-entrypoint.sh
-RUN mkdir -p "$PG_DATA"
-RUN chmod 775 -R "$PG_DATA"
-RUN chown postgres:postgres "$PG_DATA"
+#RUN mkdir -p $PG_BASE
+#RUN chmod 700 -R $PG_BASE
+#RUN chown -v postgres:postgres $PG_BASE
 
-ADD docker-entrypoint.sh /docker-entrypoint.sh
-RUN chmod 775 /docker-entrypoint.sh
-#RUN chmod -v +x /docker-entrypoint.sh
+#RUN mkdir -p "$PG_DATA"
+#RUN chmod 777 -R "$PG_DATA"
+#RUN chown -v postgres:postgres "$PG_DATA"
+	
+RUN mkdir -p "$PG_DATA" && \
+    chown -R postgres:postgres "$PG_DATA" && \
+    chmod 700 "$PG_DATA" # this 777 will be replaced by 700 at runtime (allows semi-arbitrary "--user" values)
 
-USER postgres
-ENTRYPOINT [ "/docker-entrypoint.sh" ]
+VOLUME $PG_DATA
+#VOLUME [/var/lib/postgresql/data]
+
+#ADD docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
+COPY docker-entrypoint.sh /usr/local/bin/
+RUN ln -s /usr/local/bin/docker-entrypoint.sh / # backwards compat
+RUN chmod -v a+x /docker-entrypoint.sh
 
 EXPOSE 5432/tcp
-#VOLUME [ "/etc/postgresql/$PG_VERSION", "/var/log/postgresql/$PG_VERSION", "/var/lib/postgresql/$PG_VERSION" ]
-VOLUME [ "$PG_DATA" ]
 
-CMD [ "/bin/bash", "-c", "\"/usr/lib/postgresql/$PG_VERSION/bin/postgres\" -D \"$PG_DATA\" -c config_file=\"/etc/postgresql/$PG_VERSION/main/postgresql.conf\"" ]
+USER postgres
+ENTRYPOINT [ "/bin/bash", "/docker-entrypoint.sh" ]
+#CMD [ "/bin/sh", "service postgresql start" ]
+#CMD [ "/bin/bash", "-c", "service postgresql start" ]
+#CMD ["postgres"]
+CMD [ "/bin/bash", "-c", "/usr/lib/postgresql/$PG_VERSION/bin/postgres -D $PG_DATA -c config_file=\"/etc/postgresql/$PG_VERSION/main/postgresql.conf\"" ]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,52 +1,94 @@
-#!/bin/bash
+
+#/usr/bin/env bash
 # echo "local   all             postgres peer" > "/etc/postgresql/$PG_VERSION/main/pg_hba.conf"
 # echo "host all all 0.0.0.0/0 md5" > "/etc/postgresql/$PG_VERSION/main/pg_hba.conf"
 # service postgresql restart
+#!/bin/bash
 
-set -e
+#set -e
+
 
 #su postgres;
 
 #if [ `$(psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname $POSTGRES_DB -c "SELECT 1 as "exist" from pg_database WHERE datname = 'template_postgis';" | grep -q 1) != 1` ]; then
-
 #fi
 
-#if [ ! -s "$PG_DATA" ]; then
 
-if [ ! $(ls -a "$PG_DATA/wasrun") ]; then
+#_!/bin/bash
 
-service postgresql start;
 
-psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname $POSTGRES_DB <<-EOSQL
+#if [ "$1" = 'postgres' ]; then
+#    chown -R postgres "$PGDATA"
+
+#    if [ -z "$(ls -A "$PGDATA")" ]; then
+#        gosu postgres initdb
+#    fi
+
+#    exec gosu postgres "$@"
+#fi
+
+#exec "$@"
+
+#if [ "$1" = 'postgres' ]; then
+
+#fi
+set -e
+
+#if [ ! -s "$PG_RUN" ]; then
+echo "(no file?):${PG_RUN}"
+
+if [ ! $(ls -a ${PG_RUN}) ]; then
+#if [ -z "$(ls -A "$PG_DATA")" ]; then
+   
+    echo "(no):${PG_RUN}"
+
+    echo "(staing postgres)"
+    service postgresql start
+
+    echo "(alter role)"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     ALTER ROLE $POSTGRES_USER PASSWORD '$POSTGRES_PASSWORD';
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname $POSTGRES_DB <<-'EOSQL'
+    echo "(create template_postgis)"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-'EOSQL'
     CREATE DATABASE template_postgis TEMPLATE template0;
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER template_postgis <<-'EOSQL'
+    echo "(add extensions)"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" template_postgis <<-'EOSQL'
     CREATE EXTENSION plpythonu;
     CREATE EXTENSION "uuid-ossp";
     CREATE EXTENSION fuzzystrmatch;
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER template_postgis -f /usr/share/postgresql/${PG_VERSION}/contrib/postgis-$POSTGIS_VERSION/postgis.sql; #>/dev/null;
-psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER template_postgis -f /usr/share/postgresql/${PG_VERSION}/contrib/postgis-$POSTGIS_VERSION/spatial_ref_sys.sql; #>/dev/null
+    echo "(run postgis files)"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" template_postgis -f /usr/share/postgresql/${PG_VERSION}/contrib/postgis-$POSTGIS_VERSION/postgis.sql #>/dev/null;
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" template_postgis -f /usr/share/postgresql/${PG_VERSION}/contrib/postgis-$POSTGIS_VERSION/spatial_ref_sys.sql #>/dev/null
 
-psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname $POSTGRES_DB <<-'EOSQL'
+    echo "(set database as template)";
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-'EOSQL'
     UPDATE pg_database SET datistemplate=true WHERE datname='template_postgis';
 EOSQL
 
-cat /dev/null > "$PG_DATA/wasrun";
-chown postgres:postgres "$PG_DATA/wasrun";
-chmod 775 "$PG_DATA/wasrun";
+    #cat "none" > "${PG_DATA}/wasrun";
+    #touch "${PG_DATA}/wasrun";
+    #chmod 777 "${PG_DATA}/wasrun";
 
-#cat "none" > "${PG_DATA}/wasrun";
-#touch "${PG_DATA}/wasrun";
-#chmod 777 "${PG_DATA}/wasrun";
+    cat /dev/null > "${PG_RUN}"
+    echo "(created): ${PG_RUN}"
+    #chown postgres:postgres "${PG_DATA}/wasrun";
+    #chmod a+rwx "${PG_DATA}/wasrun";
 
-service postgresql stop;
+    echo "stop service ..."
+    service postgresql stop
+    
+    echo "xxxxx"
+    exec "$@"
+    
+    #cat /dev/null > "$PG_DATA/wasrun";
+    #chown postgres:postgres "$PG_DATA/wasrun";
+    #chmod 775 "$PG_DATA/wasrun";
 
 fi
 
